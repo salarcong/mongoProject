eaefeb2a3b81afaa3a8a964721bcbd4d18fc8d79
salarcong <106347308+salarcong@users.noreply.github.com>
2023-12-28 01:06:10 +0000

    save

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 from decouple import config
 
 
-URL = 'mongodb+srv://sebsler08:zKHqZJEzRInAQlFw@cluster0.trgd3sm.mongodb.net/?retryWrites=true&w=majority'
+URL = 'mongodb+srv://sebsler08:{}@cluster0.trgd3sm.mongodb.net/?retryWrites=true&w=majority'.format(
+    config('MONGODB_PASSWORD', default='zKHqZJEzRInAQlFw')
+)
